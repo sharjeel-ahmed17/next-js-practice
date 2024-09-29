496b83c727f7b01062353981263cf471c801dea4
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-09-29 22:54:38 +0500

    para

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
     return (
         <>
             <h1>Home Page</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci voluptatum repellendus minima a aliquid eligendi quas, nulla molestiae exercitationem, voluptas soluta repudiandae blanditiis? Explicabo quas itaque error distinctio quasi est!</p>
         </>
     )
 }
